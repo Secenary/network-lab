85010831cd10d6f9f1a9c5e8d25b37b6a204cfc7
Secenary <Secenary@gmail.com>
2025-05-31 21:22:13 +0800

    implemented eth_out function

--- include/config.h
+++ include/config.h
@@ -8,16 +8,16 @@
     }  // 测试用网卡ip地址
 #define NET_IF_MAC                         \
     {                                      \
-        0x11, 0x22, 0x33, 0x44, 0x55, 0x66 \
+        0x58, 0x11, 0x22, 0xDE, 0xFF, 0xF3 \
     }  // 测试用网卡mac地址
 #else
 #define NET_IF_IP        \
     {                    \
-        XX, XX, XX, XX \
+        192, 168, 56, 100 \
     }  // 自定义网卡ip地址
 #define NET_IF_MAC                         \
     {                                      \
-        0x00, 0x11, 0x22, 0x33, 0x44, 0x55 \
+        0x11, 0x22, 0x33, 0x44, 0x55, 0x66 \
     }  // 自定义网卡mac地址
 #endif
 
